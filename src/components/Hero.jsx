db619816b8e0aa47444f81a67e16c26f358471b6
sergioortega18 <sergio_ortega70232@elpoli.edu.co>
2024-12-16 17:17:39 -0500

    Optimizacion Hero-Navbar Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center sm:flex-row '>
+    <div className='flex flex-col items-center sm:flex-row'>
       {/* Hero Left Side */}
       <div className='w-full sm:w-1/2 flex items-center justify-center py-2 sm:py-0'>
             
@@ -17,18 +17,16 @@ const Hero = () => {
                 <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-tight p-4'> Escala las Ventas de tu Negocio de Manera Rentable</h1>
                 
                 <div className='flex items-center gap-2 px-4'>   
-                    <p className=' font-medium text-base md:text-base lead'>DESDE EL CONCEPTO HASTA LA POSPRODUCCIÓN </p>
-                    <p className='w-16 md:w-11 h-[2px] bg-[#414141]'></p>
+                    <p className=' font-medium text-sm md:text-base lead'>DESDE EL CONCEPTO HASTA LA POSPRODUCCIÓN </p>
+                    <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
                 </div>
 
-                <div className='flex items-center gap-2 px-4 lg:p-6 p-4'>
-                    <button type='submit' className='bg-black text-white text-sm px-10 py-4'>ESCRIBENOS</button>
+                <div className='flex items-center gap-2 px-4 lg:p-6 p-6'>
+                    <button type='submit' className='bg-black text-white text-xs px-6 py-3 rounded'>ESCRIBENOS</button>
                 </div>
 
             </div>
       </div>
-
-
       {/* Hero Right Side */}
       <img className='w-full sm:w-1/2 h-1/4' src={assets.hero_img} alt="" />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
     const [visible, setVisible] = useState(false);
 
     return (
+
         <div className="flex items-center justify-between py-5 font-medium">
 
             {/*primera Seccion */}
@@ -15,6 +16,7 @@ const Navbar = () => {
 
             {/* Segunda Seccion */}
             <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+
                 <NavLink to ='/ignite/' className='flex flex-col items-center gap-1'>
                     <p>HOME</p>
                     <hr className="W-2/4 border-none h-[1.5px] bg-gray-700 hidden"></hr>
@@ -34,40 +36,33 @@ const Navbar = () => {
 
             </ul>
 
-            <div className='flex items-center gap-6'>
-                <img  src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
-            
-                <div className='group relative'>
-                    <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
-                {/* Dropdown Menu */}
-                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
-                    <div className='flex flex-col gap-2 w-36 py-3 px-5  bg-slate-100 text-gray-500 rounded'>
-                        <p className='cursor-pointer hover:text-black'>My Profile</p>
-                        <p className='cursor-pointer hover:text-black'>Orders</p>
-                        <p className='cursor-pointer hover:text-black'>Logout</p>
-                    </div>
+                <div className='hidden sm:flex items-center gap-2 px-4 lg:p-6 p-4 '>
+                    <button type='submit' className='bg-black text-white text-xs px-6 py-3 rounded'>ESCRIBENOS</button>
                 </div>
-            </div> 
-            <Link to='/' className='relative'>
-                <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
-                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'> 3 </p>
-            </Link> 
+            
+
             <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" /> 
-      </div>
+      
 
         {/* Sidebar menu for small screens */}
-        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
-                <div className='flex flex-col text-gray-600'>
-                    <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
-                        <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
-                        <p>Back</p>
+            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+                    <div className='flex flex-col text-gray-600'>
+                        
+                        <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                            <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+                            <p>Back</p>
+                        </div>
+
+                        <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/ignite/'>HOME</NavLink>
+                        <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/services'>SERVICES</NavLink>
+                        <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                        <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+                        
+                        <div className='flex items-center gap-2 px-4 lg:p-6 p-4 '>
+                            <button type='submit' className='bg-black text-white text-xs px-2 py-3 rounded'>ESCRIBENOS</button>
+                        </div>
                     </div>
-                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/ignite/'>HOME</NavLink>
-                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/services'>SERVICES</NavLink>
-                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
-                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
-                </div>
-        </div>
+            </div>
 
         </div>
     )
